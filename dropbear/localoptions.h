3842fcaf6a02ad6313f6c088b70598553a3562ec
Kamil Amanowicz <kamil.amanowicz@phoenix-rtos.com>
2019-04-05 17:40:49 +0200

    dropbear: generate keys to /local

--- dropbear/localoptions.h
+++ dropbear/localoptions.h
@@ -3,4 +3,8 @@
 
 #include <sys/select.h>
 
+#define DSS_PRIV_FILENAME "/local/dropbear_dss_host_key"
+#define RSA_PRIV_FILENAME "/local/dropbear_rsa_host_key"
+#define ECDSA_PRIV_FILENAME "/local/dropbear_ecdsa_host_key"
+
 #endif
