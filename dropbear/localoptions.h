49d3e5af306c47a7149da04e39c6699c2e97d9c2
Jan Sikorski <jan.sikorski@phoenix-rtos.com>
2019-08-09 12:03:41 +0200

    dropbear: allow use of DROPBEAR_PASSWORD env variable

--- dropbear/localoptions.h
+++ dropbear/localoptions.h
@@ -6,5 +6,6 @@
 #define DSS_PRIV_FILENAME "/local/dropbear_dss_host_key"
 #define RSA_PRIV_FILENAME "/local/dropbear_rsa_host_key"
 #define ECDSA_PRIV_FILENAME "/local/dropbear_ecdsa_host_key"
+#define DROPBEAR_USE_PASSWORD_ENV 1
 
 #endif
