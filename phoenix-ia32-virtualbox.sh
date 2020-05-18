f38e1632ccbaa42edf42966a9a99d885375f05a1
Pawel Pisarczyk <pawel.pisarczyk@phoesys.com>
2020-05-18 20:33:58 +0200

    Adding riscv-pk - for building bbl loader and linking it with Phoenix-RTOS kernel to run on Spike and qemu emulators

--- phoenix-ia32-virtualbox.sh
+++ phoenix-ia32-virtualbox.sh
@@ -2,28 +2,33 @@
 
 set -e
 
-VM="phoenix-ia32"
+VM="phoenix-ia32-generic"
 
 create_vm() {
 	echo -e "\033[1;33m--- Creating ${VM} Virtualbox VM ---\033[0m"
 
 	pushd _boot/
 	# Convert raw ext2 image to vdi
-	VBoxManage convertfromraw "$VM.ext2" "$VM.vdi"
+#	VBoxManage convertfromraw "$VM.disk" "$VM.vdi"
 
 	# Create and register VM
-	VBoxManage createvm --name $VM --ostype "Other" --register
+#	VBoxManage createvm --name $VM --ostype "Other" --register
 
 	# Add IDE Controller with created vdi disk attached
-	VBoxManage storagectl $VM --name "IDE Controller" --add ide --controller ICH6
+#	VBoxManage storagectl $VM --name "IDE Controller" --add ide --controller ICH6
 	VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium "$VM.vdi"
 
 	# Modify misc system settings
-	VBoxManage modifyvm $VM --ioapic on
-	VBoxManage modifyvm $VM --boot1 disk --boot2 none --boot3 none --boot4 none
-	VBoxManage modifyvm $VM --memory 64 --vram 32
+#	VBoxManage modifyvm $VM --ioapic on
+#	VBoxManage modifyvm $VM --boot1 disk --boot2 none --boot3 none --boot4 none
+#	VBoxManage modifyvm $VM --memory 64 --vram 32
 	popd
 }
+
+
+create_vm
+exit 0
+
 
 if [ "$(VBoxManage list vms | grep -c -e $VM)" == "1" ]; then
 	if [ "$(VBoxManage showvminfo $VM | grep -c "running (since")" == "1" ]; then
