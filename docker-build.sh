d34955f736f7b0b97a0a789b87861446488b554b
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2020-10-22 16:41:12 +0200

    build: docker: macOS: use tmpfs for intermediate build artifacts
    I/O operations on bind mounts in Darwin are painfully slow, speed up the
    building.

--- docker-build.sh
+++ docker-build.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 DOCKER_IMG_NAME=phoenixrtos/build
 DOCKER_USER="$(id -u):$(id -g)"
+
+TMPFS_OVERLAY=()
+if [ "$(uname)" = "Darwin" ]; then
+    # I/O operations on bind mounts in Darwin are painfully slow - use tmpfs for intermediate build artifacts
+    chmod 777 "_build"  # fix against invalid tmpfs permissions
+    TMPFS_OVERLAY=("--tmpfs"  "/src/_build")
+fi
 
 if [ "$#" -eq 1 ] && [ "$1" = "bash" ]; then
     # run interactive shell - using ROOT user
     exec docker run -it  --rm -v "$(pwd):/src" -w /src -e TARGET --entrypoint bash $DOCKER_IMG_NAME
 else
     # run build - use our own UID/GID to create files with correct owner
-    exec docker run -it --user "$DOCKER_USER" --rm -v "$(pwd):/src:delegated" -w /src -e TARGET $DOCKER_IMG_NAME "$@"
+    exec docker run -it --user "$DOCKER_USER" --rm -v "$(pwd):/src:delegated" -w /src "${TMPFS_OVERLAY[@]}" -e TARGET $DOCKER_IMG_NAME "$@"
 fi
 
