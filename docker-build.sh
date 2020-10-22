949cb5788171c6f4477673890f9b4f60c420fb51
Marek Białowąs <marek.bialowas@phoenix-rtos.com>
2020-10-22 13:49:48 +0200

    build: docker: bind share as delegated
    Should result in better I/O disk performance.

--- docker-build.sh
+++ docker-build.sh
@@ -8,6 +8,6 @@ if [ "$#" -eq 1 ] && [ "$1" = "bash" ]; then
     exec docker run -it  --rm -v "$(pwd):/src" -w /src -e TARGET --entrypoint bash $DOCKER_IMG_NAME
 else
     # run build - use our own UID/GID to create files with correct owner
-    exec docker run -it --user "$DOCKER_USER" --rm -v "$(pwd):/src" -w /src -e TARGET $DOCKER_IMG_NAME "$@"
+    exec docker run -it --user "$DOCKER_USER" --rm -v "$(pwd):/src:delegated" -w /src -e TARGET $DOCKER_IMG_NAME "$@"
 fi
 
