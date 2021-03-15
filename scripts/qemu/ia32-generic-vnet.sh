e64a37ea23e9194144ad6a3269aa18787cbce2a2
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2021-03-15 18:41:22 +0100

    Added scripts to simplify running Phoenix-RTOS on QEMU
    JIRA: PD-20

---
+++ scripts/qemu/ia32-generic-vnet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Shell script for running Phoenix-RTOS on QEMU with attached VirtIO network card device (ia32-generic)
+#
+# Copyright 2021 Phoenix Systems
+# Author: Lukasz Kosinski
+#
+
+if [ "$#" -eq 0 ]; then
+	NETDEVS="-vnet user"
+else
+	NETDEVS="-vnet $*"
+fi
+
+exec bash "$(dirname "$BASH_SOURCE")/ia32-generic.sh" $NETDEVS
