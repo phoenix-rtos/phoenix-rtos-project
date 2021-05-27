7012ba6290322b1f71c4e54e0b916b8f43153ea1
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2021-05-27 09:47:52 +0200

    project: add example of user app
    JIRA: PD-106

---
+++ _user/hello/main.c
@@ -0,0 +1,24 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Hello World
+ *
+ * Example of user application
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Hubert Buczynski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	printf("Hello World!!\n");
+
+	return 0;
+}
