01308312cfc9d83bb81d97027eaba41feed249b7
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2021-06-15 13:28:20 +0200

    _user/rotrectangle: add rotating rectangle example - draft
    JIRA: PD-119

--- _user/rotrectangle/main.c
+++ _user/rotrectangle/main.c
@@ -14,12 +14,12 @@
  */
 
 
-/*					a
-*	(x0,y0)	.................
-*			|				|
-*			|		.		|
-*		b	|	(xc,yc)		|
-*			|...............|
+/*                 a
+*     (x0,y0) ...............
+*           |               |
+*           |      .        |
+*         b |   (xc,yc)     |
+*           |...............|
 */
 
 #include <stdio.h>
@@ -32,6 +32,7 @@
 
 #include <sys/threads.h>
 
+
 typedef struct {
 	unsigned int xc;
 	unsigned int yc;
@@ -39,6 +40,7 @@ typedef struct {
 	unsigned int b;
 	unsigned int color;
 } rectangle_t;
+
 
 typedef struct {
 	rectangle_t rec;
@@ -51,6 +53,12 @@ typedef struct {
 	char stop;
 } rotrectangle_t;
 
+
+struct {
+	char stack[1024] __attribute__((aligned(8)));
+} rotrectangle_common;
+
+
 /* Enables/disables canon mode and echo */
 static int rotrectangle_switchmode(int canon)
 {
@@ -60,13 +68,14 @@ static int rotrectangle_switchmode(int canon)
 	if (canon) {
 		state.c_lflag |= ICANON;
 		state.c_lflag |= ECHO;
-	} else {
+	}
+	else {
 		state.c_lflag &= ~ICANON;
 		state.c_lflag &= ~ECHO;
 		state.c_cc[VMIN] = 1;
 	}
 	tcsetattr(STDIN_FILENO, TCSANOW, &state);
-	
+
 	return 0;
 }
 
@@ -76,26 +85,26 @@ static void rotrectangle_getkey(void *arg)
 	char ch;
 	rotrectangle_t *self = (rotrectangle_t *)arg;
 	while ((self->stop == 0) && (ch = getchar())) {
-		if (ch == 27) {					/*ESC code from arrow keys*/
+		if (ch == 27) { /*ESC code from arrow keys*/
 			getchar();
 			ch = getchar();
 		}
 		switch (ch) {
 			case 67:
-				if (self->rec.xc < (self->maxxc-2))
-					self->rec.xc+=3;
+				if (self->rec.xc < (self->maxxc - 2))
+					self->rec.xc += 3;
 				break;
 			case 68:
-				if (self->rec.xc > (self->minxc+2))
-					self->rec.xc-=3;
+				if (self->rec.xc > (self->minxc + 2))
+					self->rec.xc -= 3;
 				break;
 			case 65:
-				if (self->rec.yc > (self->minyc+2))
-					self->rec.yc-=3;
+				if (self->rec.yc > (self->minyc + 2))
+					self->rec.yc -= 3;
 				break;
 			case 66:
-				if (self->rec.yc < (self->maxyc-2))
-					self->rec.yc+=3;
+				if (self->rec.yc < (self->maxyc - 2))
+					self->rec.yc += 3;
 				break;
 			case 43:
 				if (self->rotSpeed < 5)
@@ -123,46 +132,48 @@ static void rotrectangle_getkey(void *arg)
 	}
 }
 
+
 static int rotrectangle_print(rectangle_t *self, graph_t *graphp, unsigned int angle)
 {
 	double dx, dy, angleRad, xcDouble, ycDouble, aDouble, bDouble;
-	unsigned int x0, y0, x, y;		/* (x,y) is start point for next rectangle lines */
+	unsigned int x0, y0, x, y; /* (x,y) is start point for next rectangle lines */
 
 	xcDouble = (double)self->xc;
 	ycDouble = (double)self->yc;
 	aDouble = (double)self->a;
 	bDouble = (double)self->b;
 
-	angleRad = ((double)(angle)*3.141592)/180.0;
-	x0 = (unsigned int)(xcDouble - ( (sin(M_PI/2-angleRad-atan(bDouble/aDouble)))*sqrt(pow(aDouble, 2.0)+pow(bDouble, 2.0)) )/2.0);
-	y0 = (unsigned int)(ycDouble - ( (cos(M_PI/2-angleRad-atan(bDouble/aDouble)))*sqrt(pow(aDouble, 2.0)+pow(bDouble, 2.0)) )/2.0);
+	angleRad = ((double)(angle)*M_PI) / 180.0;
+	x0 = (unsigned int)(xcDouble - ((sin(M_PI / 2 - angleRad - atan(bDouble / aDouble))) * sqrt(pow(aDouble, 2.0) + pow(bDouble, 2.0))) / 2.0);
+	y0 = (unsigned int)(ycDouble - ((cos(M_PI / 2 - angleRad - atan(bDouble / aDouble))) * sqrt(pow(aDouble, 2.0) + pow(bDouble, 2.0))) / 2.0);
 
 	x = x0;
 	y = y0;
-	dx = (self->a)*cos(angleRad); 
-	dy = (self->a)*sin(angleRad);
+	dx = (self->a) * cos(angleRad);
+	dy = (self->a) * sin(angleRad);
 	graph_line(graphp, x, y, (int)dx, (int)dy, 1, self->color, GRAPH_QUEUE_HIGH);
 
-	x = x0+dx;
-	y = y0+dy;
-	dx = (self->b)*cos(angleRad+(0.5*3.141592));
-	dy = (self->b)*sin(angleRad+(0.5*3.141592));
+	x = x0 + dx;
+	y = y0 + dy;
+	dx = (self->b) * cos(angleRad + (0.5 * M_PI));
+	dy = (self->b) * sin(angleRad + (0.5 * M_PI));
 	graph_line(graphp, x, y, (int)dx, (int)dy, 1, self->color, GRAPH_QUEUE_HIGH);
 
 	x = x0;
 	y = y0;
-	dx = (self->b)*cos(angleRad+(0.5*3.141592)); 
-	dy = (self->b)*sin(angleRad+(0.5*3.141592));
+	dx = (self->b) * cos(angleRad + (0.5 * M_PI));
+	dy = (self->b) * sin(angleRad + (0.5 * M_PI));
 	graph_line(graphp, x, y, (int)dx, (int)dy, 1, self->color, GRAPH_QUEUE_HIGH);
 
-	x = x0+dx;
-	y = y0+dy;
-	dx = (self->a)*cos(angleRad); 
-	dy = (self->a)*sin(angleRad);
+	x = x0 + dx;
+	y = y0 + dy;
+	dx = (self->a) * cos(angleRad);
+	dy = (self->a) * sin(angleRad);
 	graph_line(graphp, x, y, (int)dx, (int)dy, 1, self->color, GRAPH_QUEUE_HIGH);
 
 	return 0;
 }
+
 
 static int rotrectangle_save(rotrectangle_t *self)
 {
@@ -175,25 +186,28 @@ static int rotrectangle_save(rotrectangle_t *self)
 	return 0;
 }
 
+
 static int rotrectangle_rotating(rotrectangle_t *self, graph_t *graphp)
 {
 	unsigned int alfa, ret;
 	ret = 0;
 	while (self->stop == 0) {
-		for (alfa=0; alfa<360; alfa+=self->rotSpeed) {
+		for (alfa = 0; alfa < 360; alfa += self->rotSpeed) {
 			if (self->stop != 0)
 				break;
 			ret = rotrectangle_print(&(self->rec), graphp, alfa);
+			/* filling rectangle functions are commented out, because of problem with screen refreshing, it's demo without it */
 			// graph_fill(graphp, self->rec.xc, self->rec.yc, self->rec.color, GRAPH_FILL_FLOOD, GRAPH_QUEUE_HIGH);
 			ret = rotrectangle_save(self);
-			usleep(10000);		/* min value */
+			usleep(10000); /* min value */
 			ret = rotrectangle_print(&(self->toRemove), graphp, alfa);
 			// graph_fill(graphp, self->rec.xc, self->rec.yc, 0, GRAPH_FILL_FLOOD, GRAPH_QUEUE_HIGH);
 		}
 	}
-	
+
 	return ret;
 }
+
 
 static int rotrectangle_startpanel(rotrectangle_t *self)
 {
@@ -215,10 +229,10 @@ static int rotrectangle_startpanel(rotrectangle_t *self)
 	return 0;
 }
 
+
 int main(void)
 {
 	int ret;
-	char stack[1024] __attribute__ ((aligned(8)));
 
 	graph_adapter_t adapter = GRAPH_ANY;
 	graph_mode_t mode = GRAPH_DEFMODE;
@@ -259,11 +273,11 @@ int main(void)
 		return ret;
 	}
 
-	rotrectangle.minyc = rotrectangle.minxc = (unsigned int)(sqrt(pow((double) rotrectangle.rec.a, 2.0)+pow((double) rotrectangle.rec.b, 2.0))/2)+2;
-	rotrectangle.maxyc = graph.height-rotrectangle.minyc;
-	rotrectangle.maxxc = graph.width-rotrectangle.minxc;
+	rotrectangle.minyc = rotrectangle.minxc = (unsigned int)(sqrt(pow((double)rotrectangle.rec.a, 2.0) + pow((double)rotrectangle.rec.b, 2.0)) / 2) + 2;
+	rotrectangle.maxyc = graph.height - rotrectangle.minyc;
+	rotrectangle.maxxc = graph.width - rotrectangle.minxc;
 
-	ret = beginthread(rotrectangle_getkey, 4, stack, sizeof(stack), &rotrectangle);
+	ret = beginthread(rotrectangle_getkey, 4, rotrectangle_common.stack, sizeof(rotrectangle_common.stack), &rotrectangle);
 
 	ret = rotrectangle_rotating(&rotrectangle, &graph);
 
