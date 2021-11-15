426298f1aec0950734bb8a60c70cef407c4a8f32
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2021-11-15 12:35:04 +0100

    flash-image-zynq7000: move image creation from script to building process
    JIRA: PD-203

--- scripts/armv7a9-zynq7000.sh
+++ scripts/armv7a9-zynq7000.sh
@@ -7,21 +7,15 @@
 #
 
 IMG_PLO_ZYNQ7000="$(dirname "${BASH_SOURCE[0]}")/../_boot/plo-armv7a9-zynq7000-qemu.img"
-IMG_FLASH_QEMU="$(dirname "${BASH_SOURCE[0]}")/../_build/armv7a9-zynq7000/prog/flash-armv7a9-zynq7000.bin"
-IMG_PHOENIX_ZYNQ7000="$(dirname "${BASH_SOURCE[0]}")/../_boot/phoenix-armv7a9-zynq7000.disk"
+IMG_FLASH_QEMU="$(dirname "${BASH_SOURCE[0]}")/../_boot/flash-armv7a9-zynq7000.bin"
 DTB_ZYNQ7000="$(dirname "${BASH_SOURCE[0]}")/../scripts/zynq7000-zc702.dtb"
 
-for FILE in "$IMG_PLO_ZYNQ7000" "$IMG_PHOENIX_ZYNQ7000" "$DTB_ZYNQ7000"; do
-    if [ ! -f "$FILE" ]; then
-        echo "Missing required file: $FILE"
-        exit 1
-    fi
+for FILE in "$IMG_PLO_ZYNQ7000" "$IMG_FLASH_QEMU" "$DTB_ZYNQ7000"; do
+	if [ ! -f "$FILE" ]; then
+		echo "Missing required file: $FILE"
+		exit 1
+	fi
 done
-
-# Create file as counterpart of NOR flash memory
-rm -f "$IMG_FLASH_QEMU"
-dd if="$IMG_PHOENIX_ZYNQ7000" of="$IMG_FLASH_QEMU" bs=4M
-truncate -s 16M "$IMG_FLASH_QEMU"
 
 exec qemu-system-aarch64 \
 	-M arm-generic-fdt-7series \
