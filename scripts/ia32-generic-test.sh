fd3e7ae467d77ced52bc98609b14ef4e1f0c8beb
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2022-03-25 12:15:36 +0100

    scripts: add ia32-generic script with net setup for testing purposes
    JIRA: CI-116

---
+++ scripts/ia32-generic-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Shell script for running Phoenix-RTOS on QEMU for testing purposes (ia32-generic)
+#
+# Copyright 2022 Phoenix Systems
+# Author: Damian Loewnau
+#
+
+exec qemu-system-i386 \
+	-hda "$(dirname "${BASH_SOURCE[0]}")/../_boot/phoenix-ia32-generic.disk" \
+	-nographic \
+	-monitor none \
+	-netdev user,id=net0 -device rtl8139,netdev=net0 "$@"
