e421edaa129ab989c2d631763d5c4af74e2be364
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2022-06-24 15:30:14 +0200

    _projects/armv7a9-zynq7000: add GPIO configuration
    JIRA: PP-46

--- _projects/armv7a9-zynq7000-qemu/board_config.h
+++ _projects/armv7a9-zynq7000-qemu/board_config.h
@@ -69,4 +69,62 @@
 #define SPI1_SS1  -1
 #define SPI1_SS2  -1
 
+/* GPIO bank 0 configuration */
+#define GPIO0_0  -1
+#define GPIO0_1  -1
+#define GPIO0_2  -1
+#define GPIO0_3  -1
+#define GPIO0_4  -1
+#define GPIO0_5  -1
+#define GPIO0_6  -1
+#define GPIO0_7  -1
+#define GPIO0_8  -1
+#define GPIO0_9  -1
+#define GPIO0_10 -1
+#define GPIO0_11 -1
+#define GPIO0_12 -1
+#define GPIO0_13 -1
+#define GPIO0_14 -1
+#define GPIO0_15 -1
+#define GPIO0_16 -1
+#define GPIO0_17 -1
+#define GPIO0_18 -1
+#define GPIO0_19 -1
+#define GPIO0_20 -1
+#define GPIO0_21 -1
+#define GPIO0_22 -1
+#define GPIO0_23 -1
+#define GPIO0_24 -1
+#define GPIO0_25 -1
+#define GPIO0_26 -1
+#define GPIO0_27 -1
+#define GPIO0_28 -1
+#define GPIO0_29 -1
+#define GPIO0_30 -1
+#define GPIO0_31 -1
+
+/* GPIO bank 1 configuration */
+#define GPIO1_0  -1
+#define GPIO1_1  -1
+#define GPIO1_2  -1
+#define GPIO1_3  -1
+#define GPIO1_4  -1
+#define GPIO1_5  -1
+#define GPIO1_6  -1
+#define GPIO1_7  -1
+#define GPIO1_8  -1
+#define GPIO1_9  -1
+#define GPIO1_10 -1
+#define GPIO1_11 -1
+#define GPIO1_12 -1
+#define GPIO1_13 -1
+#define GPIO1_14 -1
+#define GPIO1_15 -1
+#define GPIO1_16 -1
+#define GPIO1_17 -1
+#define GPIO1_18 -1
+#define GPIO1_19 -1
+#define GPIO1_20 -1
+#define GPIO1_21 -1
+
 #endif
