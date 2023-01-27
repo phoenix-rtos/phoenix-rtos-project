b11855d6f66e9ef1c2759f4e54ae5fd815563aff
Hubert Badocha <hubert.badocha@phoenix-rtos.com>
2023-01-27 16:02:58 +0100

    Add support for QSPI flashnor.
    JIRA: RTOS-320

--- _projects/armv7a7-imx6ull-evk/board_config.h
+++ _projects/armv7a7-imx6ull-evk/board_config.h
@@ -3,8 +3,8 @@
  *
  * Board config for armv7a7-imx6ull-evk
  *
- * Copyright 2022 Phoenix Systems
- * Author: Lukasz Kosinski
+ * Copyright 2022, 2023 Phoenix Systems
+ * Author: Lukasz Kosinski, Hubert Badocha
  *
  * This file is part of Phoenix-RTOS.
  *
@@ -13,5 +13,10 @@
 
 #ifndef _BOARD_CONFIG_H_
 #define _BOARD_CONFIG_H_
+
+#define QSPI_FLASH_A1_SIZE (256 * 1024 * 1024)
+#define QSPI_FLASH_A2_SIZE (0)
+#define QSPI_FLASH_B1_SIZE (0)
+#define QSPI_FLASH_B2_SIZE (0)
 
 #endif
