3b4ab87f5a8f0304dc4c221ac28951f0a360f4ed
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2023-06-07 14:52:47 +0200

    add sparcv8leon3-gr716-mini project
    JIRA: RTOS-251

---
+++ _projects/sparcv8leon3-gr716-mini/board_config.h
@@ -0,0 +1,69 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Board config for sparcv8leon3-gr716-mini
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Lukasz Leczkowski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _BOARD_CONFIG_H_
+#define _BOARD_CONFIG_H_
+
+
+#define UART_MAX_CNT 6
+
+#define UART_BAUDRATE 115200
+
+#define UART0_BASE ((void *)0x80300000)
+#define UART1_BASE ((void *)0x80301000)
+#define UART2_BASE ((void *)0x80302000)
+#define UART3_BASE ((void *)0x80303000)
+#define UART4_BASE ((void *)0x80304000)
+#define UART5_BASE ((void *)0x80305000)
+
+#define UART0_IRQ 24
+#define UART1_IRQ 25
+#define UART2_IRQ 3
+#define UART3_IRQ 5
+#define UART4_IRQ 6
+#define UART5_IRQ 7
+
+#define UART0_IRQ_LINE 24
+#define UART1_IRQ_LINE 25
+#define UART2_IRQ_LINE 42
+#define UART3_IRQ_LINE 44
+#define UART4_IRQ_LINE 45
+#define UART5_IRQ_LINE 46
+
+#define UART0_TX 2
+#define UART0_RX 3
+
+#define UART1_TX 4
+#define UART1_RX 5
+
+#define UART2_TX 58
+#define UART2_RX 59
+
+#define UART3_TX 62
+#define UART3_RX 61
+
+#define UART4_TX 38
+#define UART4_RX 39
+
+#define UART5_TX 43
+#define UART5_RX 44
+
+#define UART_CONSOLE_PLO    2
+#define UART_CONSOLE_KERNEL 2
+#define UART_CONSOLE_USER   2
+
+#define GPIO_DIR_IN  0
+#define GPIO_DIR_OUT 1
+
+
+#endif
