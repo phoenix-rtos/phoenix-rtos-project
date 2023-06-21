f986d5b7e52edae5943f6309ba1330058cf43094
DawidSzpejna <dawid.szpejna@phoenix-rtos.com>
2023-06-21 10:24:32 +0200

    project: add exmple of user app in cpp
    JIRA: RTOS-405

---
+++ _user/hellocpp/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Hello World++
+ *
+ * Example of user application
+ *
+ * Copyright 2023 Phoenix Systems
+ * Author: Dawid Szpejna
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <iostream>
+
+
+int main(void)
+{
+	std::cout << "Hello World++!" << std::endl;
+
+	return 0;
+}
