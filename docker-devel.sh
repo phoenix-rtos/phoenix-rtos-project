5661715c5aa76b77dd12d7bb9ee607e9efe49849
Hubert Badocha <hubert.badocha@phoenix-rtos.com>
2023-07-21 13:45:18 +0200

    Enable docker devel to be run from any directory
    JIRA: RTOS-527

--- docker-devel.sh
+++ docker-devel.sh
@@ -2,12 +2,12 @@
 #
 # Shell script for running the devel docker with arguments passed to the script
 #
-# Copyright 2021 Phoenix Systems
-# Author: Hubert Buczynski
+# Copyright 2021, 2023 Phoenix Systems
+# Author: Hubert Buczynski, Hubert Badocha
 #
 
 DOCKER_IMG_NAME=phoenixrtos/devel
-PATH_TO_PROJECT="$(dirname "$(pwd)/${BASH_SOURCE[0]}")/"
+PATH_TO_PROJECT="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/"
 
 if [ "$#" -eq 0 ]; then
     exec docker run -it --privileged --rm -v /dev/bus/usb/:/dev/bus/usb -v "${PATH_TO_PROJECT}:/project" -w /project $DOCKER_IMG_NAME bash
