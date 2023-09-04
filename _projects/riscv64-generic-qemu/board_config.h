9e6ad56e27d61d774b331ac17adb4cbf9eeb2b5d
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2023-09-04 17:09:56 +0200

    riscv64: update for PLO use
    JIRA: RTOS-550

--- _projects/riscv64-generic-qemu/board_config.h
+++ _projects/riscv64-generic-qemu/board_config.h
@@ -17,8 +17,17 @@
 /* UART16550 configuration */
 #define UART16550_CONSOLE_USER 0
 #define UART16550_BAUDRATE     115200
+#define UART16550_IRQ          10
+#define UART16550_BASE         0x10000000
 
 /* SPIKE-TTY configuration */
 #define SPIKETTY_CONSOLE_USER 0
+
+#define TIMER_FREQ (10 * 1000 * 1000)
+
+#define RAM_ADDR      0x20000000u
+#define RAM_BANK_SIZE 0x10000000u
+
+#define PLIC_IRQ_SIZE 54
 
 #endif
