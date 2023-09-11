1482843cfff890a1c3a2d5cbdfd8b70d149d22c1
Damian <damian.loewnau@phoenix-rtos.com>
2023-09-11 18:28:40 +0200

    mtd-utils: switch to building if files don't exist (macOS)
    JIRA: CI-340

--- mtd-utils/build.sh
+++ mtd-utils/build.sh
@@ -6,12 +6,12 @@ if [ "$(uname)" = "Darwin" ]; then
 	b_log "Building mtd-utils"
 
 	PREFIX_MTD_UTILS="${PREFIX_PROJECT}/mtd-utils"
-	PREFIX_MTD_UTILS_BUILD="${PREFIX_PROJECT}/_build/host-generic-pc/mtd-utils"
+	PREFIX_MTD_UTILS_BUILD="${PREFIX_BUILD_HOST}/mtd-utils"
 
 	mkdir -p "$PREFIX_MTD_UTILS_BUILD"
 
 	pushd "$PREFIX_MTD_UTILS_BUILD"
-	if [ -n "$CLEAN" ]; then
+	if [ ! -f "${PREFIX_BUILD_HOST}/prog.stripped/mkfs.jffs2" ]; then
 		tar xzvf "$PREFIX_MTD_UTILS/mtd-utils-patched.tar.gz"
 
 		if [ -f Makefile ]; then
@@ -24,9 +24,9 @@ if [ "$(uname)" = "Darwin" ]; then
 
 	make mkfs.jffs2
 
-	mkdir -p "${PREFIX_PROJECT}/_build/host-generic-pc/prog"
-	mkdir -p "${PREFIX_PROJECT}/_build/host-generic-pc/prog.stripped"
-	cp -a mkfs.jffs2 "${PREFIX_PROJECT}/_build/host-generic-pc/prog/"
-	cp -a mkfs.jffs2 "${PREFIX_PROJECT}/_build/host-generic-pc/prog.stripped/"
+	mkdir -p "${PREFIX_BUILD_HOST}/prog"
+	mkdir -p "${PREFIX_BUILD_HOST}/prog.stripped"
+	cp -a mkfs.jffs2 "${PREFIX_BUILD_HOST}/prog/"
+	cp -a mkfs.jffs2 "${PREFIX_BUILD_HOST}/prog.stripped"
 	popd
 fi
