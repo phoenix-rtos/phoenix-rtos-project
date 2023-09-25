cc74810eadd5a241126a0d080ac6bcc008317bcf
Hubert Badocha <hubert.badocha@phoenix-rtos.com>
2023-09-25 20:25:25 +0200

    Exec stack

--- _user/hello/main.c
+++ _user/hello/main.c
@@ -15,10 +15,29 @@
 
 #include <stdio.h>
 
-
 int main(void)
 {
-	printf("Hello World!!\n");
+	void *ptr = (void *)0x1;
 
-	return 0;
+	printf("ret = %d\n", sscanf("(nil)", "%p", &ptr));
+	printf("ptr = %p\n", ptr);
+	/* printf("ret = %d\n", sscanf("(nil) ", "%p", &ptr));
+	printf("ptr = %p\n", ptr);
+	printf("ret = %d\n", sscanf("(nil) 42", "%p", &ptr));
+	printf("ptr = %p\n", ptr); */
 }
+
+
+	/* void *ptr = (void *)0x1;
+	int xd = 0x1;
+	printf("ptr = %p, %d\n", ptr, xd);
+	printf("%d\n", sscanf("(nil) 42", "%p", &ptr));
+	printf("ptr = %p, %d, %d\n", ptr, xd, errno);
+	printf("%d\n", sscanf("(nil) ", "%p", &ptr));
+	printf("ptr = %p, %d, %d\n", ptr, xd, errno);
+	printf("WORKED BEFORE%d\n", sscanf("(nil)", "%p", &ptr));
+	printf("ptr = %p, %d, %d\n", ptr, xd, errno);
+	printf("%d\n", sscanf("(nil) 42", "%p %d", &ptr, &xd));
+	printf("ptr = %p, %d, %d\n", ptr, xd, errno);
+	sscanf("0 42", "%p %d", &ptr, &xd);
+	printf("ptr = %p, %d\n", ptr, xd); */
--- scripts/ia32-generic-qemu.sh
+++ scripts/ia32-generic-qemu.sh
@@ -12,4 +12,5 @@ exec qemu-system-i386 \
 	-serial stdio \
 	-vga cirrus \
 	-drive "file=$(dirname "${BASH_SOURCE[0]}")/../_boot/ia32-generic-qemu/phoenix.disk,format=raw,media=disk,index=0" \
-	-netdev user,id=net0 -device rtl8139,netdev=net0 "$@"
+	-netdev user,id=net0 -device rtl8139,netdev=net0 "$@" \
+	-s -S
