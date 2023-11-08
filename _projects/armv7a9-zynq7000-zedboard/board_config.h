a7b4b8f9a26415581395b508bb0b6b9aa5aff585
Maska989 <damian.modzelewski@phoenix-rtos.com>
2023-11-08 13:32:08 +0100

    armv7a9: change usb-UART to MIO UART

--- _projects/armv7a9-zynq7000-zedboard/board_config.h
+++ _projects/armv7a9-zynq7000-zedboard/board_config.h
@@ -82,9 +82,9 @@
 #define UART1_RX 49
 #define UART1_TX 48
 
-#define UART_CONSOLE_KERNEL 1
-#define UART_CONSOLE_USER   1
-#define UART_CONSOLE_PLO    1
+#define UART_CONSOLE_KERNEL 0
+#define UART_CONSOLE_USER   0
+#define UART_CONSOLE_PLO    0
 
 
 /* UART16550 configuration */
