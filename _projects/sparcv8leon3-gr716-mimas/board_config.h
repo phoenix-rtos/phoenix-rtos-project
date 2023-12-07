140fa1f76c5a4b97bbf02d10f83b1c742d40a564
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2023-12-07 17:13:12 +0100

    sparcv8leon3: adapt to use of amba pnp
    JIRA: RTOS-704

--- _projects/sparcv8leon3-gr716-mimas/board_config.h
+++ _projects/sparcv8leon3-gr716-mimas/board_config.h
@@ -16,58 +16,57 @@
 
 #define SYSCLK_FREQ (100 * 1000 * 1000) /* MHz */
 
-#define UART_MAX_CNT 6
+#define UART_MAX_CNT 2
 
 #define UART_BAUDRATE 115200
 
-#define UART0_BASE ((void *)0x80300000)
-#define UART1_BASE ((void *)0x80301000)
-#define UART2_BASE ((void *)0x80302000)
-#define UART3_BASE ((void *)0x80303000)
-#define UART4_BASE ((void *)0x80304000)
-#define UART5_BASE ((void *)0x80305000)
+#define UART0_BASE ((void *)0x80302000)
+#define UART1_BASE ((void *)0x80303000)
+#define UART2_BASE ((void *)-1)
+#define UART3_BASE ((void *)-1)
+#define UART4_BASE ((void *)-1)
+#define UART5_BASE ((void *)-1)
 
-#define UART0_IRQ 24
-#define UART1_IRQ 25
-#define UART2_IRQ 3
-#define UART3_IRQ 5
-#define UART4_IRQ 6
-#define UART5_IRQ 7
+#define UART0_IRQ 3
+#define UART1_IRQ 5
+#define UART2_IRQ -1
+#define UART3_IRQ -1
+#define UART4_IRQ -1
+#define UART5_IRQ -1
 
-#define UART0_TX 2
-#define UART0_RX 3
+#define UART0_TX 58
+#define UART0_RX 59
 
-#define UART1_TX 4
-#define UART1_RX 5
+#define UART1_TX 62
+#define UART1_RX 61
 
-#define UART2_TX 58
-#define UART2_RX 59
+#define UART2_TX -1
+#define UART2_RX -1
 
-#define UART3_TX 62
-#define UART3_RX 61
+#define UART3_TX -1
+#define UART3_RX -1
 
-#define UART4_TX 38
-#define UART4_RX 39
+#define UART4_TX -1
+#define UART4_RX -1
 
-#define UART5_TX 43
-#define UART5_RX 44
+#define UART5_TX -1
+#define UART5_RX -1
 
-#define UART0_ACTIVE 0
-#define UART1_ACTIVE 0
-#define UART2_ACTIVE 1
-#define UART3_ACTIVE 1
+#define UART0_ACTIVE 1
+#define UART1_ACTIVE 1
+#define UART2_ACTIVE 0
+#define UART3_ACTIVE 0
 #define UART4_ACTIVE 0
 #define UART5_ACTIVE 0
 
-#define UART_CONSOLE_PLO    3
-#define UART_CONSOLE_KERNEL 3
-#define UART_CONSOLE_USER   3
+#define UART_CONSOLE_PLO    1
+#define UART_CONSOLE_KERNEL 1
+#define UART_CONSOLE_USER   1
 
 #define GPIO_DIR_IN  0
 #define GPIO_DIR_OUT 1
 
 #define PSEUDODEV 1
-
 
 /* Interrupts */
 
@@ -81,6 +80,28 @@
 #define TIMER_IRQ     9
 #define GPTIMER0_BASE ((void *)0x80003000)
 #define GPTIMER1_BASE ((void *)0x80004000)
+
+/* ADC */
+
+#define ADC_CNT 0
+
+#define ADC0_ACTIVE 0
+#define ADC1_ACTIVE 0
+#define ADC2_ACTIVE 0
+#define ADC3_ACTIVE 0
+#define ADC4_ACTIVE 0
+#define ADC5_ACTIVE 0
+#define ADC6_ACTIVE 0
+#define ADC7_ACTIVE 0
+
+/* SPI */
+
+#define SPI_CNT 1
+#define GRLIB_SPI_LOOPBACK
+
+/* GPIO */
+
+#define GPIO_PORT_CNT 0
 
 
 #endif
