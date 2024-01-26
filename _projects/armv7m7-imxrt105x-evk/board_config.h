befbc9b3ea8314445b054145131517d75bacd254
Gerard Swiderski <gerard@swiderski.eu>
2024-01-26 13:05:34 +0100

    imxrt: enable libposixsrv on rt1167, rt1064
    Enable posixsrv on i.MX RT1176 and RT1064 by default,
    keep libpseudodev on i.MX RT1052 due to low amount of ram.
    
    JIRA: RTOS-641

--- _projects/armv7m7-imxrt105x-evk/board_config.h
+++ _projects/armv7m7-imxrt105x-evk/board_config.h
@@ -14,6 +14,18 @@
 #ifndef _BOARD_CONFIG_H_
 #define _BOARD_CONFIG_H_
 
+
+/*
+ * libpseudodev and libposixsrv shall be used exclusively, libpseudodev uses
+ * less resources, but libposixsrv provides POSIX support and may be resource
+ * hungry.
+ * NOTICE: Due to low amount of OCRAM on mimxrt1052-evk by default libpseudodev
+ * is enabled.
+ */
+
 #define PSEUDODEV 1
+
+/* #define BUILTIN_POSIXSRV 1 */
+
 
 #endif
