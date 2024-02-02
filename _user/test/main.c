3f4008c8688336f21c1209f6ea48e30b7e3f6211
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2024-02-02 16:55:37 +0100

    test

---
+++ _user/test/main.c
@@ -0,0 +1,24 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Hello World
+ *
+ * Example of user application
+ *
+ * Copyright 2021 Phoenix Systems
+ * Author: Hubert Buczynski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	for (;;) sleep(1);
+
+	return 0;
+}
