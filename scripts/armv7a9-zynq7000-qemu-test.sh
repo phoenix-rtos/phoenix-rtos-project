3952534bec303593c886ca15a085e66ea019fc25
Maska989 <Hootwheels989@gmail.com>
2024-02-02 09:03:27 +0100

    scripts: add zynq7000-qemu-test script
    Removing capability of controlling qemu via shortcuts, which is needed for control codes test.
    
    JIRA: CI-397

---
+++ scripts/armv7a9-zynq7000-qemu-test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Shell script for running phoenix-rtos loader on Qemu fork from Xilinx (zynq-7000)
+#
+# Copyright 2021 Phoenix Systems
+# Author: Hubert Buczynski, Damian Modzelewski
+#
+
+IMG_PLO_ZYNQ7000="$(dirname "${BASH_SOURCE[0]}")/../_boot/armv7a9-zynq7000-qemu/plo.img"
+IMG_FLASH_QEMU="$(dirname "${BASH_SOURCE[0]}")/../_boot/armv7a9-zynq7000-qemu/phoenix.disk"
+DTB_ZYNQ7000="$(dirname "${BASH_SOURCE[0]}")/../scripts/zynq7000-zc702.dtb"
+
+for FILE in "$IMG_PLO_ZYNQ7000" "$IMG_FLASH_QEMU" "$DTB_ZYNQ7000"; do
+	if [ ! -f "$FILE" ]; then
+		echo "Missing required file: $FILE"
+		exit 1
+	fi
+done
+
+exec qemu-system-aarch64 \
+	-M arm-generic-fdt-7series \
+	-dtb "$DTB_ZYNQ7000" \
+	-serial null \
+	-serial stdio \
+	-device loader,file="$IMG_PLO_ZYNQ7000" \
+	-drive file="$IMG_FLASH_QEMU",if=mtd,format=raw,index=0
