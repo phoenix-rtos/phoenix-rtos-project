ddc4f634d81f48f1d233ba2c26f41ff73d08f86a
Andrzej Głowiński <andrzej.glowinski@phoenix-rtos.com>
2024-03-26 12:23:19 +0100

    DO_NOT_MERGE: Show case of decreasing of plo size

---
+++ _projects/armv7m4-stm32l4x6-nucleo_min_plo/board_config.h
@@ -0,0 +1,24 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Board config for armv7m4-stm32l4x6-nucleo
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Lukasz Kosinski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _BOARD_CONFIG_H_
+#define _BOARD_CONFIG_H_
+
+#define TTY2         1
+#define UART_CONSOLE 2
+#define TTY2_DMA     1
+
+#define BUILTIN_POSIXSRV 1
+
+
+#endif
