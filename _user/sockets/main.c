1ff4558237b9754ab28dfdfc58378004d6126e79
Adam Greloch <adam.greloch@phoenix-rtos.com>
2024-07-23 15:16:39 +0200

    expand for non-blocking cases, reformat

--- _user/sockets/main.c
+++ _user/sockets/main.c
@@ -20,7 +20,37 @@
 #include <sys/un.h>
 #include <string.h>
 #include <poll.h>
+#include <fcntl.h>
+#include <stdbool.h>
 #include "err.h"
+
+/* use to check whether accept() is indeed non-blocking */
+bool client_noop = false;
+
+/* enable to use fcntl for setting up non-blocking flag  */
+bool use_fcntl = true;
+
+bool nonblocking_client = true;
+bool nonblocking_server = true;
+
+int create_socket(bool nonblocking)
+{
+	int sock, flags;
+	if (nonblocking && use_fcntl) {
+		CHECK_ERRNO(sock = socket(AF_UNIX, SOCK_STREAM, PF_UNSPEC));
+		flags = fcntl(sock, F_GETFL, 0);
+		if (flags < 0) {
+			printf("fcntl() error");
+			exit(EXIT_FAILURE);
+		}
+		CHECK_ERRNO(fcntl(sock, F_SETFL, flags | O_NONBLOCK));
+	}
+	else {
+		int flags = nonblocking ? SOCK_NONBLOCK : 0;
+		CHECK_ERRNO(sock = socket(AF_UNIX, SOCK_STREAM | flags, PF_UNSPEC));
+	}
+	return sock;
+}
 
 int main(void)
 {
@@ -40,44 +70,113 @@ int main(void)
   CHECK_ERRNO(pid = fork());
 
   if (pid == 0) {
-    CHECK_ERRNO(datasock = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, PF_UNSPEC));
-    sleep(1);
-    k = connect(datasock, (const struct sockaddr *) &addr, sizeof(struct sockaddr_un));
-    if (k < 0 && errno == EINPROGRESS)
-      printf("connect() didn't block!\n");
+	  if (client_noop) {
+		  printf("client no-op\n");
+		  exit(EXIT_SUCCESS);
+	  }
 
-    int timeout_msecs = 500;
-    struct pollfd pfd[1];
+	  datasock = create_socket(nonblocking_client);
 
-    pfd[0].fd = datasock;
-    pfd[0].events = POLLOUT;
+	  while (true) {
+		  k = connect(datasock, (const struct sockaddr *)&addr, sizeof(struct sockaddr_un));
+		  if (k < 0 && errno == EINPROGRESS) {
+			  printf("connect() didn't block!\n");
+			  break;
+		  }
+		  else if (k == 0) {
+			  printf("connect() connected!\n");
+			  break;
+		  }
+		  else if (k < 0) {
+			  printf("connect() error: %s\n", strerror(errno));
+			  sleep(1);
+			  printf("connect() retrying\n");
+		  }
+	  }
 
-    CHECK_ERRNO(poll(pfd, 1, timeout_msecs));
+	  int timeout_msecs = 500;
+	  struct pollfd pfd[1];
 
-    if (pfd[0].revents & POLLOUT) {
-      printf("poll(): socket ready to write\n");
-    }
-    if (pfd[0].revents & POLLHUP) {
-      printf("poll(): connection disconnected\n");
-    }
+	  pfd[0].fd = datasock;
+	  pfd[0].events = POLLOUT;
 
+	  printf("client is now polling\n");
+	  while (true) {
+		  CHECK_ERRNO(poll(pfd, 1, timeout_msecs));
+		  if (pfd[0].revents & POLLOUT) {
+			  printf("client POLLOUT: socket ready to write\n");
+			  char *msg = "i am a message";
+			  CHECK_ERRNO(send(datasock, msg, BUF_SIZE, 0));
+			  printf("client sent message\n");
+			  break;
+		  }
+		  else if (pfd[0].revents & POLLHUP) {
+			  printf("client POLLHUP\n");
+		  }
+		  else {
+			  printf("client poll timeouted\n");
+		  }
+	  }
+	  printf("client exited\n");
+
+	  // FIXME graceful exit
   } else {
-    CHECK_ERRNO(sock = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, PF_UNSPEC));
-    CHECK_ERRNO(bind(sock, (const struct sockaddr *) &addr, sizeof(struct sockaddr_un)));
-    CHECK_ERRNO(listen(sock, 0));
+	  sock = create_socket(nonblocking_server);
 
-    datasock = accept(sock, NULL, NULL);
-    if (datasock < 0) {
-      printf("accept() didn't block and returned: %s\n", strerror(errno));
-    } else {
-      printf("accept(): connection estabilished\n");
-    }
+	  CHECK_ERRNO(bind(sock, (const struct sockaddr *)&addr, sizeof(struct sockaddr_un)));
+	  CHECK_ERRNO(listen(sock, 0));
 
-    CHECK_ERRNO(read(datasock, buf, BUF_SIZE));
-    buf[BUF_SIZE-1] = 0;
-    printf("read(): got '%s'\n", buf);
+	  do {
+		  if (!nonblocking_server) {
+			  datasock = accept(sock, NULL, NULL);
 
-    wait(NULL);
+			  if (datasock <= 0) {
+				  printf("accept() didn't block and returned: %s\n", strerror(errno));
+				  break;
+			  }
+			  printf("socket ready to read/write\n");
+			  CHECK_ERRNO(read(datasock, buf, BUF_SIZE));
+			  buf[BUF_SIZE - 1] = 0;
+			  printf("read(): got '%s'\n", buf);
+		  }
+		  else {
+			  int timeout_msecs = 500;
+			  struct pollfd pfd[1];
+
+			  pfd[0].fd = sock;
+			  pfd[0].events = POLLIN;
+
+			  printf("server is now polling\n");
+			  while (true) {
+				  CHECK_ERRNO(poll(pfd, 1, timeout_msecs));
+
+				  if ((pfd[0].revents & POLLIN)) {
+					  printf("server POLLIN!");
+					  CHECK_ERRNO(datasock = accept(sock, NULL, NULL));
+					  if (datasock > 0) {
+						  printf("server has accepted connection!\n");
+
+						  CHECK_ERRNO(read(datasock, buf, BUF_SIZE));
+						  buf[BUF_SIZE - 1] = 0;
+						  printf("read(): got '%s'\n", buf);
+					  }
+					  else {
+						  printf("server failed to accept()\n");
+					  }
+					  break;
+				  }
+				  else if (pfd[0].revents & POLLHUP) {
+					  printf("server POLLHUP");
+				  }
+				  else {
+					  printf("server poll timeouted\n");
+				  }
+			  }
+		  }
+		  printf("server exited\n");
+	  } while (0);
+
+	  wait(NULL);
   }
 
 	return 0;
