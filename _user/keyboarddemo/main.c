f2ccae3267c867d08c2e11af9010237ed14985ba
Adam Greloch <adam.greloch@phoenix-rtos.com>
2024-08-22 10:44:26 +0200

    add keyboard/mouse demos

---
+++ _user/keyboarddemo/main.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <poll.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int fd = open("/dev/kbd", O_RDONLY);
+
+	unsigned char c;
+	int rv;
+
+	while (1) {
+		if ((rv = read(fd, &c, 1)) > 0) {
+			if (c & 0x80) {
+				c &= 0x7f;
+				printf("%u - released\n", c);
+			}
+			else
+				printf("%u - pressed\n", c);
+			fflush(stdout);
+		}
+		if (rv < 0)
+			printf("%s", strerror(errno));
+		usleep(200);
+	}
+}
