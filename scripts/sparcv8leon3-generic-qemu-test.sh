e1fe6da070d62bcda71a134b70ac308fd993d638
Maska989 <Hootwheels989@gmail.com>
2024-08-28 15:52:14 +0200

    scripts: add sparcv8leon3-generic-qemu-test script
    JIRA: CI-474

---
+++ scripts/sparcv8leon3-generic-qemu-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Shell script for running Phoenix-RTOS on sparcv8leon-generic-qemu
+#
+# Copyright 2024 Phoenix Systems
+# Author: Lukasz Leczkowski
+#
+
+IMG_PLO_LEON3="$(dirname "${BASH_SOURCE[0]}")/../_boot/sparcv8leon3-generic-qemu/plo.elf"
+IMG_FLASH_QEMU="$(dirname "${BASH_SOURCE[0]}")/../_boot/sparcv8leon3-generic-qemu/phoenix.disk"
+
+exec qemu-system-sparc \
+	-smp 1 \
+	-m 128M \
+	-M leon3_generic \
+	-serial mon:stdio \
+	-nographic \
+	-monitor none \
+	-kernel "$IMG_PLO_LEON3" \
+	-device loader,file="$IMG_FLASH_QEMU",addr=0x47e00000,force-raw=on
