adbc59265562e98eb5adf8b4507309fd4b725305
Hubert Badocha <hubert.badocha@phoenix-rtos.com>
2024-09-14 11:25:36 +0200

    Add dynamic linking support
    JIRA: RTOS-664

---
+++ _user/dlopen/hello.h
@@ -0,0 +1,21 @@
+/*
+ * Phoenix-RTOS
+ *
+ * dlopen
+ *
+ * Example of user application using dlopen.
+ *
+ * Copyright 2024 Phoenix Systems
+ * Author: Hubert Badocha
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _USER_DLOPEN_DYN_H_
+#define _USER_DLOPEN_DYN_H_
+
+int hello(void);
+
+#endif
