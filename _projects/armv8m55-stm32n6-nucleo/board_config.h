edac483711ef0f754696367b00b0374b1932e411
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2025-06-13 10:52:59 +0200

    Add armv8m55-stm32n6-nucleo target
    JIRA: RTOS-1068

---
+++ _projects/armv8m55-stm32n6-nucleo/board_config.h
@@ -0,0 +1,40 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Board config for armv8m55-stm32n6-nucleo
+ *
+ * Copyright 2024, 2025 Phoenix Systems
+ * Author: Aleksander Kaminski, Jacek Maksymowicz
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _BOARD_CONFIG_H_
+#define _BOARD_CONFIG_H_
+
+
+#define UART_IO_PORT gpioe
+#define UART_IO_AF   7
+#define UART_PIN_TX  5
+#define UART_PIN_RX  6
+
+#define TTY1                1
+#define UART_CONSOLE_PLO    1
+#define UART_CONSOLE_KERNEL 1
+#define UART_CONSOLE_USER   1
+
+/*
+ * libpseudodev and libposixsrv shall be used exclusively, libpseudodev uses
+ * less resources, but libposixsrv provides POSIX support and may be resource
+ * hungry, by default libposixsrv is enabled.
+ */
+
+/* #define PSEUDODEV 1 */
+#define BUILTIN_POSIXSRV 1
+#define BUILTIN_DUMMYFS  1
+
+/* plo params */
+
+#endif
