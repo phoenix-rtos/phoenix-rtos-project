dcc4dc7c914995c3d8bc61d13fab54e7680f421b
Mateusz Kobak <mateusz.kobak@phoenix-rtos.com>
2025-08-22 11:03:38 +0200

    _user/cognit_app/scripts: Update scripts to HEM v2.1.0

--- _user/cognit_app/scripts/decision.py
+++ _user/cognit_app/scripts/decision.py
@@ -1,35 +1,39 @@
 def A(timestamp,s3_parameters,besmart_parameters,home_model_parameters,storage_parameters,ev_battery_parameters_per_id,heating_parameters,user_preferences):
-	n=Exception
-	N=len
+	u=Exception
+	O=len
 	C=int
-	A6='curr_charge_level';A5='energy_consumption';A4='Not enough data for decision-making';A3='origin';A2='datetime64[m]';A1='datetime64[ns]';A0='get_last';z='till';y='since';x='Authorization';w='workspace_key';v='X-Auth';m='OutWRte';l='nominal_power';c=user_preferences;b='StorCtl_Mod';a='InWRte';Z='Accept';Y='Content-Type';P=heating_parameters;O=storage_parameters;M=.0;K=home_model_parameters;H=ev_battery_parameters_per_id;G=s3_parameters;F='application/json';D=besmart_parameters;import datetime as d,json as E;from io import BytesIO;import boto3,numpy as A,onnx,torch as I,requests as Q;from onnx2torch import convert
-	def A7(actor_model,tensor_state,lower_bounds,upper_bounds):
+	A9='pv_generation';A8='curr_charge_level';A7='Not enough data for decision-making';A6='origin';A5='get_last';A4='Authorization';A3='workspace_key';A2='X-Auth';t='OutWRte';s='nominal_power';r='energy_consumption';q=True;p=False;d=user_preferences;c='StorCtl_Mod';b='InWRte';a='Accept';Z='Content-Type';Q=heating_parameters;P=storage_parameters;N=.0;M='s';I=ev_battery_parameters_per_id;H=home_model_parameters;G=s3_parameters;F='application/json';D=besmart_parameters;import datetime as e,json as E;from io import BytesIO;import boto3,numpy as A,onnx,torch as J,requests as R;from onnx2torch import convert
+	def AA(actor_model,tensor_state,lower_bounds,upper_bounds):
 		E=lower_bounds;D=tensor_state;C=upper_bounds
-		with I.no_grad():D=I.FloatTensor(D).to(AB);B=actor_model(D)
+		with J.no_grad():D=J.FloatTensor(D).to(AE);B=actor_model(D)
 		B=B.detach().cpu().numpy().flatten();B[0]=B[0]*(C[0]-E[0])/2+(C[0]+E[0])/2;B[1]=B[1]*C[1]
-		for F in range(N(J)):B[2+F]=(B[2+F]+1)*C[2+F]/2
+		for F in range(O(K)):B[2+F]=(B[2+F]+1)*C[2+F]/2
 		B=A.clip(B,A.array(E),A.array(C));return B
-	def A8():B='password';A='login';C={Y:F,Z:F,v:D[w]};E={A:D[A],B:D[B]};G=Q.post('https://api.besmart.energy/api/users/token',headers=C,json=E);return G.json()['token']
-	def A9(cid,mid,moid):D={Y:F,Z:F,x:f"Bearer {r}"};E=A.datetime64(B)+A.timedelta64(R,'s');G=[{'client_cid':cid,'sensor_mid':mid,'signal_type_moid':moid,y:C(A.datetime64(B).astype(C)/1000),z:C(E.astype(C)/1000),A0:True}];H=Q.post('https://api.besmart.energy/api/sensors/signals/data',headers=D,json=G);return H.json()[0]['data']
-	def o(identifier):
-		B=identifier;D=A9(B['cid'],B['mid'],B['moid']);G=(A.array(D['time'])*1e6).astype(C).astype(A1).astype(A2);H=A.array(D['value']);E=A.array(D[A3]);F=H[E==2];I=G[E==2]
-		if N(F)<2:raise n(A4)
-		return F
-	def AA():
-		E={Y:F,Z:F,x:f"Bearer {r}"};H=D[A5];I=Q.get(f"https://api.besmart.energy/api/sensors/{H['cid']}.{H['mid']}",headers=E).json();E={Y:F,Z:F,v:D[w]};L=A.datetime64(B)+A.timedelta64(R,'s');M={y:C(A.datetime64(B).astype(C)/1000),z:C(L.astype(C)/1000),'delta_t':R//60,'raw':False,A0:True};O=Q.get(f"https://api.besmart.energy/api/weather/{I['lat']}/{I['lon']}/{D['temperature_moid']}/data",headers=E,params=M);G=O.json()['data'];P=(A.array(G['time'])*1e6).astype(C).astype(A1).astype(A2);S=A.array(G['value']);J=A.array(G[A3]);K=S[J==3];T=P[J==3]
-		if N(K)<2:raise n(A4)
-		return K-272.15
-	AB=I.device('cuda'if I.cuda.is_available()else'cpu');G=E.loads(G);D=E.loads(D);K=E.loads(K);O=E.loads(O);H=E.loads(H);P=E.loads(P);c=E.loads(c);B=d.datetime.fromtimestamp(timestamp);R=c['cycle_timedelta_s'];e=R//60;S=B.minute%e
-	if S>e/2:S=-(e-S)
-	B=d.datetime(year=B.year,month=B.month,day=B.day,hour=B.hour,minute=B.minute);B=B-d.timedelta(minutes=S);T=K['min_temp_setting'];U=K['max_temp_setting'];V=O[l];AC=O[A6];J=list(H.keys());J.sort();f=P['curr_temp'];p=P['preferred_temp'];q=K['heating_delta_temperature'];AD=[T,-V]+N(J)*[M];AE=[U,V]+[H[A][l]for A in J];r=A8();AF=o(D['pv_generation'])[0];g=o(D[A5]);g=A.diff(g)[0];AG=AA()[0];AH=boto3.client('s3',endpoint_url=G['endpoint_url'],aws_access_key_id=G['access_key_id'],aws_secret_access_key=G['secret_access_key']);h=BytesIO();AH.download_fileobj(Bucket=G['bucket_name'],Key=G['model_filename'],Fileobj=h);h.seek(0);AI=onnx.load_model_from_string(h.getvalue());AJ=convert(AI);s=(B.hour+B.minute/60)/24,AF/3,g/3,(f-T)/U,(f-(p-q))/(U-T),(p+q-f)/(U-T),AG/30,AC/100
-	for W in J:i=H[W];AK=i['is_available'];AL=i['time_until_charged']/3600;AM=i[A6];s+=AK,AL/24,AM/100
-	j=A7(AJ,I.tensor(s,dtype=I.float).unsqueeze(0),AD,AE);AN=j[0];k=j[1];AO=j[2:];L={a:M,m:M}
-	if k>0:L[a]=k/V*1e2;L[b]=1
-	else:L[m]=-k/V*1e2;L[b]=2
-	t={}
-	for(W,u)in zip(J,AO):
-		AP=H[W][l];X={a:M,m:M}
-		if u>0:X[a]=u/AP*1e2;X[b]=1
-		else:X[b]=0
-		t[W]=X
-	return E.dumps({'temp':AN}),E.dumps(L),E.dumps(t)
+	def AB():B='password';A='login';C={Z:F,a:F,A2:D[A3]};E={A:D[A],B:D[B]};G=R.post('https://api.besmart.energy/api/users/token',headers=C,json=E);return G.json()['token']
+	def AC(cid,mid,moid,is_cumulative=p):
+		E={Z:F,a:F,A4:f"Bearer {y}"};D=A.datetime64(C(B.timestamp()),M)
+		if is_cumulative:D-=A.timedelta64(3600,M)
+		G=A.datetime64(C(B.timestamp()),M)+A.timedelta64(S,M);H=[{'client_cid':cid,'sensor_mid':mid,'signal_type_moid':moid,'since':C(D.astype(C)*1000),'till':C(G.astype(C)*1000),A5:q}];I=R.post('https://api.besmart.energy/api/sensors/signals/data',headers=E,json=H);return I.json()[0]['data']
+	def v(identifier,is_cumulative=p):
+		I=is_cumulative;G=identifier;H=AC(G['cid'],G['mid'],G['moid'],I);K=A.array(H['value']);J=A.array(H[A6]);D=K[J==2]
+		if I:E=(A.array(H['time'])/1e3).astype(C)[J==2];F=A.where(E>=B.timestamp())[0][0];D=D[F-1:F+1];E=E[F-1:F+1];D=A.diff(D)/(A.diff(E)/3600)
+		if O(D)<1:raise u(A7)
+		return D[0]
+	def AD():
+		E={Z:F,a:F,A4:f"Bearer {y}"};G=D[r];H=R.get(f"https://api.besmart.energy/api/sensors/{G['cid']}.{G['mid']}",headers=E).json();E={Z:F,a:F,A2:D[A3]};K=A.datetime64(B)+A.timedelta64(S,M);L={'since':C(A.datetime64(B).astype(C)/1000),'till':C(K.astype(C)/1000),'delta_t':S//60,'raw':p,A5:q};N=R.get(f"https://api.besmart.energy/api/weather/{H['lat']}/{H['lon']}/{D['temperature_moid']}/data",headers=E,params=L);I=N.json()['data'];P=A.array(I['value']);Q=A.array(I[A6]);J=P[Q==3]
+		if O(J)<1:raise u(A7)
+		return J[0]-272.15
+	AE=J.device('cuda'if J.cuda.is_available()else'cpu');G=E.loads(G);D=E.loads(D);H=E.loads(H);P=E.loads(P);I=E.loads(I);Q=E.loads(Q);d=E.loads(d);B=e.datetime.fromtimestamp(timestamp);S=d['cycle_timedelta_s'];f=S//60;T=B.minute%f
+	if T>f/2:T=-(f-T)
+	B=e.datetime(year=B.year,month=B.month,day=B.day,hour=B.hour,minute=B.minute);B=B-e.timedelta(minutes=T);U=H['min_temp_setting'];V=H['max_temp_setting'];W=P[s];AF=P[A8];K=list(I.keys());K.sort();g=Q['curr_temp'];w=Q['preferred_temp'];x=H['temp_window'];h=H['state_range'];i=h[A9];j=h[r];k=h['temperature'];AG=[U,-W]+O(K)*[N];AH=[V,W]+[I[A][s]for A in K];y=AB();AI=v(D[A9]);AJ=v(D[r],q);AK=AD();AL=boto3.client('s3',endpoint_url=G['endpoint_url'],aws_access_key_id=G['access_key_id'],aws_secret_access_key=G['secret_access_key']);l=BytesIO();AL.download_fileobj(Bucket=G['bucket_name'],Key=G['model_filename'],Fileobj=l);l.seek(0);AM=onnx.load_model_from_string(l.getvalue());AN=convert(AM);z=(B.hour+B.minute/60)/24,(AI-i[0])/(i[1]-i[0]),(AJ-j[0])/(j[1]-j[0]),(g-U)/V,(g-(w-x))/(V-U),(w+x-g)/(V-U),(AK-k[0])/(k[1]-k[0]),AF/100
+	for X in K:m=I[X];AO=m['is_available'];AP=m['time_until_charged']/3600;AQ=m[A8];z+=AO,AP/24,AQ/100
+	n=AA(AN,J.tensor(z,dtype=J.float).unsqueeze(0),AG,AH);AR=n[0];o=n[1];AS=n[2:];L={b:N,t:N}
+	if o>0:L[b]=o/W*1e2;L[c]=1
+	else:L[t]=-o/W*1e2;L[c]=2
+	A0={}
+	for(X,A1)in zip(K,AS):
+		AT=I[X][s];Y={b:N,t:N}
+		if A1>0:Y[b]=A1/AT*1e2;Y[c]=1
+		else:Y[c]=0
+		A0[X]=Y
+	return E.dumps({'temp':AR}),E.dumps(L),E.dumps(A0)
