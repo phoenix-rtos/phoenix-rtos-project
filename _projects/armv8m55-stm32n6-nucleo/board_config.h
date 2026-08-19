5efb2d274cc52a17df2a6985bec46a7501a41317
Jacek Maksymowicz <jacek.maksymowicz@phoenix-rtos.com>
2026-08-19 17:05:09 +0200

    WIP add ethernet on nucleo and pilot
    Add board config with options
    Add lwip to syspage (arguments TBD)
    Update submodules
    
    YT: RTOS-1421

--- _projects/armv8m55-stm32n6-nucleo/board_config.h
+++ _projects/armv8m55-stm32n6-nucleo/board_config.h
@@ -26,14 +26,44 @@
 
 #define VDDIO3_RANGE_MV 1800 /* XSPI port 2 - Flash */
 
-#define I2C1 1
-
-#define SPI5 1
+// WIP Temporarily commented out to save some space in multidriver
+// #define I2C1 1
+//
+// #define SPI5 1
 
 #define TTY1                1
 #define UART_CONSOLE_PLO    1
 #define UART_CONSOLE_KERNEL 1
-#define UART_CONSOLE_USER   1
+
+#define WIPDONTMERGE_NUCLEO 0
+
+#if WIPDONTMERGE_NUCLEO
+#define UART_CONSOLE_USER 1
+#else
+#define TTY7              1
+#define UART_CONSOLE_USER 7
+#endif
+
+#if WIPDONTMERGE_NUCLEO
+#define ETH1_MDC_PORT           gpiog
+#define ETH1_MDC_PIN            11
+#define ETH1_MDIO_PORT          gpiof
+#define ETH1_MDIO_PIN           4
+#define ETH1_RX_REFCLK_INTERNAL 0
+#define ETH1_REF_CLK_PORT       gpiof
+#define ETH1_REF_CLK_PIN        7
+#define ETH1_MODE_RMII
+#else
+#define ETH1_MDC_PORT           gpioh
+#define ETH1_MDC_PIN            5
+#define ETH1_MDIO_PORT          gpiod
+#define ETH1_MDIO_PIN           12
+#define ETH1_PHY_INTN_PORT      gpiod
+#define ETH1_PHY_INTN_PIN       3
+#define ETH1_RX_REFCLK_INTERNAL 1
+#define ETH1_CLK_OUTPUT         1
+#define ETH1_MODE_RMII
+#endif
 
 /*
  * libpseudodev and libposixsrv shall be used exclusively, libpseudodev uses
